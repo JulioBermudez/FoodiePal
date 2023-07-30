0c24fe69cf4b0569897df911a32ea79090f6f3b9
rich-taveras <rtaveras@yahoo.com>
2023-07-30 12:49:46 -0400

    save work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
-var searchInput = document.getElementById("search");
 
+//to load whole DOM first
+document.addEventListener("DOMContentLoaded", function () {
+    
+//getting search word
+    var searchInput = document.getElementById("search");
+    var storedSearchTerm = localStorage.getItem('searchTerm');
+    if (storedSearchTerm) {
+      searchInput.value = storedSearchTerm;
+    }
 
+//add event listener
+  searchInput.closest("form").addEventListener("submit", function (event) {
+    event.preventDefault();
 
+//get the value from the search
+    var searchTerm = searchInput.value;
+
+//keep values on local storage
+localStorage.setItem('searchTerm', searchTerm);
+
+//redirect search to second page
+window.location.href = 'secondPage.html'
+  });
+});
